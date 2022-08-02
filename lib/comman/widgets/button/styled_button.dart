3f4156a11995f6e4151634f0f93d2c9b8a3f7a68
XxThunderBlastxX <koustavmondal55@gmail.com>
2022-08-03 02:53:34 +0530

    added signin/signup button

---
+++ lib/comman/widgets/button/styled_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class StyledButton extends StatelessWidget {
+  final String? text;
+  final Color? color;
+  final Function? onClick;
+
+  const StyledButton({
+    Key? key,
+    this.text = 'Text',
+    this.color = Colors.teal,
+    this.onClick,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      onPressed: () => onClick,
+      fillColor: color,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12.5),
+        side: const BorderSide(color: Colors.black26),
+      ),
+      child: Text(text as String),
+    );
+  }
+}
--- lib/screens/register/register_view.dart
+++ lib/screens/register/register_view.dart
@@ -1,3 +1,4 @@
+import 'package:chamting_app/comman/widgets/button/styled_button.dart';
 import 'package:fluent_ui/fluent_ui.dart';
 
 import '../../comman/widgets/windows_title_bar/windows_title_bar.dart';
@@ -10,9 +11,38 @@ class RegisterView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const ScaffoldPage(
-      padding: EdgeInsets.all(0),
-      header: WindowsTitleBar(),
+    return ScaffoldPage(
+      padding: const EdgeInsets.all(0),
+      header: const WindowsTitleBar(),
+      content: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.18),
+              child:
+                  FlutterLogo(size: MediaQuery.of(context).size.height * 0.20),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.3,
+            ),
+            Column(
+              children: [
+                StyledButton(
+                  text: "SignIn",
+                  color: Colors.teal,
+                ),
+                StyledButton(
+                  text: "SignUp",
+                  color: Colors.teal,
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
