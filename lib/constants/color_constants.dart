1151786f8385b6a714d5483fb56ad93885b8049f
SoumyadeepOSD <opencontainer255@gmail.com>
2023-02-17 15:58:52 +0530

    issue #8 color_constants.dart is created

---
+++ lib/constants/color_constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+//Primary Colors
+const red = Colors.red;
+const blue = Colors.blue;
+const yellow = Colors.yellow;
+
+//Secondary Colors
+const purple = Colors.purple;
+const orange = Colors.orange;
+const green = Colors.green;
