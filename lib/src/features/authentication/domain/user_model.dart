bc2a912e1caabcca5d11f97e45eec739e2d3ae58
SAGNIK SANYAL <sagniksanyal1502@gmail.com>
2023-03-04 00:00:44 +0530

    commented freezed for future use

--- lib/src/features/authentication/domain/user_model.dart
+++ lib/src/features/authentication/domain/user_model.dart
@@ -1,22 +1,42 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
+class User {
+  String? id;
+  String? name;
+  String? email;
+  String? password;
 
-part 'user_model.freezed.dart';
-part 'user_model.g.dart';
+  User({this.id, this.name, this.email, this.password});
 
-enum AuthProvider { google, email, apple, facebook }
+  User.fromJson(Map<String, dynamic> json) {
+    id = json['\$id'];
+    name = json['name'];
+    email = json['email'];
+    password = json['password'];
+  }
 
-@freezed
-@JsonSerializable()
-class User with _$User {
-  const factory User({
-    String? id,
-    String? name,
-    AuthProvider? authProvider,
-    String? email,
-  }) = _User;
-
-  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+  User copyWith({String? id, String? name, String? email, String? password}) {
+    return User(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      email: email ?? this.email,
+      password: password ?? this.password,
+    );
+  }
 
   @override
-  Map<String, dynamic> toJson() => _$UserToJson(this);
+  String toString() =>
+      'User(id: $id, name: $name, email: $email, password: $password)';
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    return other is User &&
+        other.id == id &&
+        other.name == name &&
+        other.email == email &&
+        other.password == password;
+  }
+
+  @override
+  int get hashCode =>
+      id.hashCode ^ name.hashCode ^ email.hashCode ^ password.hashCode;
 }
--- lib/src/providers/user_provider.dart
+++ lib/src/providers/user_provider.dart
@@ -3,25 +3,15 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class UserNotifier extends StateNotifier<User> {
   late final Ref ref;
-  UserNotifier(this.ref) : super(const User());
+  UserNotifier(this.ref) : super(User());
 
   ///Set the user
   void setUser(User user) => state = user;
 
   ///Clear the user
-  void clearUser() => state = const User();
+  void clearUser() => state = User();
 
   ///Update the useproperties
-  void updateUserProps({
-    String? id,
-    String? name,
-    AuthProvider? authProvider,
-    String? email,
-  }) =>
-      state = state.copyWith(
-        id: id,
-        name: name,
-        authProvider: authProvider,
-        email: email,
-      );
+  void updateUserProps({String? id, String? name, String? email}) =>
+      state = state.copyWith(id: id, name: name, email: email);
 }
